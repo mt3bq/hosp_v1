6d5f0f8726cb0b33c2cb859ae8e2f3a1b86e8b27
mt3bq <32024044+mt3bq@users.noreply.github.com>
2022-12-03 22:11:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ app.use(express.static('public'))
 
 
 let client= new oc({
-  url:"http://localhost:8042",
+  url:"http://ec2-3-83-82-162.compute-1.amazonaws.com:8042",
+   auth: {
+      username: 'muteb',
+      password: 'muteb'
+    }
   
   
 
